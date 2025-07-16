a18aeec5a4c8e8812704650620741482121aa390
Filip Kus <kusfilip62@gmail.com>
2025-07-16 21:26:00 +1200

    Adding functions to draw level bars for the potentiometers

--- ADSR Envelope/Firmware/adsr-envelope/src/main.rs
+++ ADSR Envelope/Firmware/adsr-envelope/src/main.rs
@@ -13,6 +13,11 @@ use heapless::{self, String};
 #[entry]
 fn main() -> ! {
     if let Some(p) = pac::Peripherals::take() {
+
+        // -------------------------------------------------------------
+        // ----------------------- STM32 Setup -------------------------
+        // -------------------------------------------------------------
+
         // Create variable for flash memory access
         let mut flash = p.FLASH;
 
@@ -36,15 +41,43 @@ fn main() -> ! {
         
         // Create struct for serial communication (USART1)
         let mut serial = Serial::usart1(p.USART1, (tx, rx), 115_200.bps(), &mut rcc);
+
            
-        
-        
-        // Draw one bar        
-        draw_line(5, 20, 10, 10, &mut serial);
-        draw_line(5, 20, 15, 15, &mut serial);
-        draw_line(20, 20, 11, 15, &mut serial);
-        draw_line(4, 4, 11, 15, &mut serial);
-        
+        // -------------------------------------------------------------
+        // ------------------------ GUI Setup --------------------------
+        // -------------------------------------------------------------
+
+        // Print module name, revision and designer
+        print("\x1b[0mEurorack Module: \x1b[2mADSR Envelope\r\n", &mut serial);
+        print("\x1b[0mHardware Revision: \x1b[2mVersion 2.0\r\n", &mut serial);
+        print("\x1b[0mDesigner: \x1b[2mFilip K\x1b[0m\r\n", &mut serial);
+
+        // Draw the attack, decay, release and sustain bars
+        draw_bar(5, 20, 5, 10, &mut serial);
+        draw_bar(5, 20, 20, 25, &mut serial);
+        draw_bar(5, 20, 35, 40, &mut serial);
+        draw_bar(5, 20, 50, 55, &mut serial);
+
+        // Add labels to bars
+        print_string_at_location("ATTACK", 5, 22, &mut serial);
+        print_string_at_location("DECAY", 20, 22, &mut serial);
+        print_string_at_location("SUSTAIN", 35, 22, &mut serial);
+        print_string_at_location("RELEASE", 50, 22, &mut serial);
+
+        // Add potentiometer levels to bars
+        print_string_at_location("30%", 6, 23, &mut serial);
+        print_string_at_location("60%", 21, 23, &mut serial);
+        print_string_at_location("10%", 36, 23, &mut serial);
+        print_string_at_location("40%", 51, 23, &mut serial);
+
+        // Add temporary bar levels
+        print("\x1b[38;5;32m", &mut serial);    // Cool blue colour
+        draw_multi_level(30, 6, 19, &mut serial); 
+        draw_multi_level(60, 21, 19, &mut serial); 
+        draw_multi_level(10, 36, 19, &mut serial); 
+        draw_multi_level(40, 51, 19, &mut serial);  
+
+        // TODO: Create class/struct for bar
 
         /*
 
@@ -63,8 +96,8 @@ fn main() -> ! {
         ATTACK
          30%       
              
-        */
-        
+        */   
+
         
     }
 
@@ -89,6 +122,29 @@ fn delay() {
     }
 }
 
+fn print_string_at_location<T>(message: &str, x: u32, y: u32, serial: &mut T)
+where 
+    T: Write<u8>,
+{
+    let mut escape_code = String::<16>::new();
+    write!(escape_code, "\x1b[{};{}H", y, x).unwrap();            
+    print(&escape_code.as_str(), serial);
+    print(message, serial);
+}
+
+fn print_u32_at_location<T>(message: u32, x: u32, y: u32, serial: &mut T)
+where 
+    T: Write<u8>,
+{
+    let mut escape_code = String::<16>::new();
+    write!(escape_code, "\x1b[{};{}H", y, x).unwrap();            
+    print(&escape_code.as_str(), serial);
+
+    let mut escape_code = String::<16>::new();
+    write!(escape_code, "{}", message).unwrap();            
+    print(&escape_code.as_str(), serial);    
+}
+
 
 fn draw_line<T>(row_init: u32, row_final: u32, col_init: u32, col_final: u32, serial: &mut T)
 where 
@@ -97,7 +153,7 @@ where
     
     if col_init == col_final {
         // Vertical direction
-        for current_row in row_init..row_final{
+        for current_row in row_init..row_final+1{
             // Print escape code            
             let mut escape_code = String::<16>::new();
             write!(escape_code, "\x1b[{};{}H", current_row, col_init).unwrap();            
@@ -109,7 +165,7 @@ where
 
     } else if row_init == row_final {
         // Horizontal direction        
-        for current_col in col_init..col_final{                        
+        for current_col in col_init..col_final+1{                        
             // Print escape code            
             let mut escape_code = String::<16>::new();
             write!(escape_code, "\x1b[{};{}H", row_init, current_col).unwrap();            
@@ -118,5 +174,60 @@ where
             // Print horizontal line            
             print("-", serial);
         }
+    }
+}
+
+fn draw_bar<T>(row_init: u32, row_final: u32, col_init: u32, col_final: u32, serial: &mut T)
+where 
+    T: Write<u8>,
+{
+
+
+    // Draw vertical lines
+    draw_line(row_init+1, row_final-1, col_init, col_init, serial);
+    draw_line(row_init+1, row_final-1, col_final, col_final, serial);
+
+    // Draw horizontal lines
+    draw_line(row_init, row_init, col_init+1, col_final-1, serial);
+    draw_line(row_final, row_final, col_init+1, col_final-1, serial);
+
+    // Draw corners
+    let mut escape_code = String::<16>::new();
+    write!(escape_code, "\x1b[{};{}H/", row_init, col_init).unwrap();            
+    print(&escape_code.as_str(), serial);
+
+    let mut escape_code = String::<16>::new();
+    write!(escape_code, "\x1b[{};{}H/", row_final, col_final).unwrap();            
+    print(&escape_code.as_str(), serial); 
+
+    let mut escape_code = String::<16>::new();
+    write!(escape_code, "\x1b[{};{}H\\", row_init, col_final).unwrap();            
+    print(&escape_code.as_str(), serial); 
+
+    let mut escape_code = String::<16>::new();
+    write!(escape_code, "\x1b[{};{}H\\", row_final, col_init).unwrap();            
+    print(&escape_code.as_str(), serial);      
+
+}
+
+fn draw_level<T>(x_init: u32, y: u32, serial: &mut T)
+where 
+    T: Write<u8>,
+{
+    print_string_at_location("#", x_init, y, serial);
+    print_string_at_location("#", x_init+1, y, serial);
+    print_string_at_location("#", x_init+2, y, serial);
+    print_string_at_location("#", x_init+3, y, serial);
+}
+
+fn draw_multi_level<T>(percentage: u32, x_init: u32, y: u32, serial: &mut T)
+where 
+    T: Write<u8>,
+{
+    // Bar height is by default 14 levels high    
+    let num_levels = percentage/7;    
+
+    for level in 0..num_levels{
+        draw_level(x_init, y-level, serial);
     }
 }
